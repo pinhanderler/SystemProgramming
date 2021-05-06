9140898b9222d45aca8c346eaa53686391f22421
Pinhan <pinhanderler@gmail.com>
2021-05-06 23:53:45 +0300

    Delete 4-27-IP-93-Uzunlu directory

--- 4-27-IP-93-Uzunlu/4-27-IP-93-Uzunlu.bat
+++
@@ -1,9 +0,0 @@
-if not exist %1.asm (
-  echo "No such file."
-  exit /b 1
-)
-
-echo "Compiling..."
-C:\masm32\bin\ml.exe -c -Zi -Fl -nologo %1.asm
-echo "Linking..."
-C:\masm32\bin\link.exe /MAP /SUBSYSTEM:CONSOLE %1.obj
