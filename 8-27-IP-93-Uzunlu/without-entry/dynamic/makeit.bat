01867b1ec774e5233923e0d0c4489071eac81a19
Pinhan <pinhanderler@gmail.com>
2021-06-20 11:39:20 +0300

    Delete 8-27-IP-93-Uzunlu directory

--- 8-27-IP-93-Uzunlu/without-entry/dynamic/makeit.bat
+++
@@ -1,4 +0,0 @@
-C:\masm32\bin\ml.exe /c /coff "mymdl.asm"
-C:\masm32\bin\link /dll /export:funcNum1 /export:funcNum2 /export:funcNum3 /noentry mymdl.obj
-C:\masm32\bin\ml.exe /c /coff "8-27-IP-93-Uzunlu.asm"
-C:\masm32\bin\link.exe /SUBSYSTEM:WINDOWS "8-27-IP-93-Uzunlu.obj"
--- 8-27-IP-93-Uzunlu/without-entry/static/makeit.bat
+++
@@ -1,4 +0,0 @@
-C:\masm32\bin\ml.exe /c /coff "mymdl.asm"
-C:\masm32\bin\link /dll /def:mymdl.def /noentry mymdl.obj
-C:\masm32\bin\ml.exe /c /coff "8-27-IP-93-Uzunlu.asm"
-C:\masm32\bin\link.exe /SUBSYSTEM:WINDOWS "8-27-IP-93-Uzunlu.obj"
